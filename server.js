be57c364959b192a22de1c99fac516a52f167e36
ybchar <ybcha@toptoon.com>
2022-06-04 15:29:17 +0900

    feat: graphql

--- server.js
+++ server.js
@@ -1,22 +1,24 @@
 import { ApolloServer, gql } from "apollo-server";
+import fetch from "node-fetch";
 
-// memory db
 let tweets = [
   {
     id: "1",
-    text: "first one",
+    text: "first one!",
+    userId: "2",
   },
   {
     id: "2",
     text: "second one",
+    userId: "1",
   },
 ];
 
 let users = [
   {
     id: "1",
-    firstName: "yb",
-    lastName: "cha",
+    firstName: "nico",
+    lastName: "las",
   },
   {
     id: "2",
@@ -25,32 +27,66 @@ let users = [
   },
 ];
 
-// graphQL
 const typeDefs = gql`
   type User {
     id: ID!
     firstName: String!
     lastName: String!
-    #dynamic field
+    """
+    Is the sum of firstName + lastName as a string
+    """
     fullName: String!
   }
+  """
+  Tweet object represents a resource for  a Tweet
+  """
   type Tweet {
     id: ID!
     text: String!
-    author: User!
+    author: User
   }
   type Query {
+    allMovies: [Movie!]!
     allUsers: [User!]!
     allTweets: [Tweet!]!
     tweet(id: ID!): Tweet
+    movie(id: String!): Movie
   }
   type Mutation {
     postTweet(text: String!, userId: ID!): Tweet!
+    """
+    Deletes a Tweet if found, else returns false
+    """
     deleteTweet(id: ID!): Boolean!
   }
+  type Movie {
+    id: Int!
+    url: String!
+    imdb_code: String!
+    title: String!
+    title_english: String!
+    title_long: String!
+    slug: String!
+    year: Int!
+    rating: Float!
+    runtime: Float!
+    genres: [String]!
+    summary: String
+    description_full: String!
+    synopsis: String
+    yt_trailer_code: String!
+    language: String!
+    background_image: String!
+    background_image_original: String!
+    small_cover_image: String!
+    medium_cover_image: String!
+    large_cover_image: String!
+  }
 `;
+// GET /api/v1/tweets
+// POST DELETE PUT /api/v1/tweets
+// GET /api/v1/tweet/:id
 
-// graphQL resolver
 const resolvers = {
   Query: {
     allTweets() {
@@ -60,8 +96,18 @@ const resolvers = {
       return tweets.find((tweet) => tweet.id === id);
     },
     allUsers() {
-      console.log("alluser called");
+      console.log("allUsers called!");
       return users;
+    },
+    allMovies() {
+      return fetch("https://yts.mx/api/v2/list_movies.json")
+        .then((r) => r.json())
+        .then((json) => json.data.movies);
+    },
+    movie(_, { id }) {
+      return fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+        .then((r) => r.json())
+        .then((json) => json.data.movie);
     },
   },
   Mutation: {
@@ -69,6 +115,7 @@ const resolvers = {
       const newTweet = {
         id: tweets.length + 1,
         text,
+        userId,
       };
       tweets.push(newTweet);
       return newTweet;
@@ -81,17 +128,19 @@ const resolvers = {
     },
   },
   User: {
-    // 원하는 쿼리 스트링을 return 받을 수 있음
     fullName({ firstName, lastName }) {
-      // console.log(firstName, lastName);
-      // return "hello";
-      return `${firstName}${lastName}`;
+      return `${firstName} ${lastName}`;
+    },
+  },
+  Tweet: {
+    author({ userId }) {
+      return users.find((user) => user.id === userId);
     },
   },
 };
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
-server.listen().then(({ url }) => {
+server.listen(3000).then(({ url }) => {
   console.log(`Running on ${url}`);
 });
